d3693795c0fe2f311379f6eb401f9cbb435ecae0
Seongsik Hwang <sean113x@unist.ac.kr>
2026-04-25 17:35:51 +0900

    maintain polymorphism

--- src/index-tree.h
+++ src/index-tree.h
@@ -5,6 +5,12 @@ protected:
     int order;
     int splitCount; // The number of split operations performed (for experiments).
 
+    int maxEntries() const {
+        return order - 1;
+    }
+
+    virtual int minEntries() const = 0;
+
 public:
     explicit IndexTree(int order)
         : order(order), splitCount(0) {}
